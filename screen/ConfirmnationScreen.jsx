1e7e976518fcf43f1e562b677f70d2b12c8158f2
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2025-07-03 14:50:48 +0530

    Address ui completed in confirmation screen

--- screen/ConfirmnationScreen.jsx
+++ screen/ConfirmnationScreen.jsx
@@ -11,6 +11,8 @@ const ConfirmnationScreen = () => {
   const [currentStep, setCurrentStep] = useState(0);
   const [addresses, setAddresses] = useState([]);
   const [selectedAddress, setSelectedAddress] = useState('');
+  const [options, setOptions] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState('');
   const { userId } = useContext(UserType);
   const steps = [
     { title: 'Address', content: 'Address Form' },
@@ -237,6 +239,151 @@ const ConfirmnationScreen = () => {
                 </>
               ))}
             </View>
+          </View>
+        )}
+
+        {currentStep == 1 && (
+          <View style={{ marginHorizontal: 20 }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+              Choose your delivery options
+            </Text>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                gap: 10,
+                borderWidth: 1,
+                borderBlockColor: '#E0E0E0',
+                padding: 10,
+                borderRadius: 10,
+                marginVertical: 20,
+              }}
+            >
+              {options ? (
+                <FontAwesome
+                  name="circle-o"
+                  size={30}
+                  onPress={() => setOptions(!options)}
+                />
+              ) : (
+                <FontAwesome
+                  onPress={() => setOptions(!options)}
+                  name="circle"
+                  size={30}
+                  color={'green'}
+                />
+              )}
+              <Text style={{ fontWeight: '500' }}>
+                <Text style={{ color: 'green' }}>Tomarrow by 10 pm - </Text>
+                Free delivery with prime membership
+              </Text>
+            </View>
+
+            <Pressable
+              onPress={() => setCurrentStep(2)}
+              style={{
+                backgroundColor: 'orange',
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+                alignItems: 'center',
+                borderRadius: 20,
+              }}
+            >
+              <Text
+                style={{ fontWeight: 'bold', color: 'white', fontSize: 18 }}
+              >
+                Continue
+              </Text>
+            </Pressable>
+          </View>
+        )}
+
+        {currentStep == 2 && (
+          <View style={{ marginHorizontal: 20 }}>
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
+              Select your Payment method
+            </Text>
+
+            <View>
+              {/* Cash Option */}
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: 10,
+                  borderWidth: 1,
+                  borderColor: '#E0E0E0',
+                  padding: 10,
+                  borderRadius: 10,
+                  marginVertical: 10,
+                }}
+              >
+                {selectedOptions === 'Cash' ? (
+                  <FontAwesome name="circle" size={24} color="green" />
+                ) : (
+                  <FontAwesome
+                    name="circle-o"
+                    size={24}
+                    color="green"
+                    onPress={() => setSelectedOptions('Cash')}
+                  />
+                )}
+                <Text style={{ fontWeight: '500' }}>Cash on Delivery</Text>
+              </View>
+
+              {/* Card Option */}
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: 10,
+                  borderWidth: 1,
+                  borderColor: '#E0E0E0',
+                  padding: 10,
+                  borderRadius: 10,
+                  marginVertical: 10,
+                }}
+              >
+                {selectedOptions === 'Card' ? (
+                  <FontAwesome name="circle" size={24} color="green" />
+                ) : (
+                  <FontAwesome
+                    name="circle-o"
+                    size={24}
+                    color="green"
+                    onPress={() => setSelectedOptions('Card')}
+                  />
+                )}
+                <Text style={{ fontWeight: '500' }}>
+                  {' '}
+                  UPI- Credit/Debit Card
+                </Text>
+              </View>
+              <Pressable
+                onPress={() => setCurrentStep(3)}
+                style={{
+                  backgroundColor: 'orange',
+                  paddingHorizontal: 20,
+                  paddingVertical: 10,
+                  alignItems: 'center',
+                  borderRadius: 20,
+                }}
+              >
+                <Text
+                  style={{ fontWeight: 'bold', color: 'white', fontSize: 18 }}
+                >
+                  Continue
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        )}
+
+        {currentStep == 3 && (
+          <View style={{ marginHorizontal: 20 }}>
+            <Text>Payment options</Text>
           </View>
         )}
       </ScrollView>
